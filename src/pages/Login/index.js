15c9029100e4968be300a4be0338b2386e6c288c
99 <3293570697@qq.com>
2022-06-02 15:42:54 +0800

    add layout-login-antd

---
+++ src/pages/Login/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import {Button} from 'antd';
+
+class Login extends Component {
+    render() {
+        return (
+            <div>
+                登录页面
+                <Button type="primary">登录按钮</Button>
+            </div>
+        );
+    }
+}
+
+export default Login;
